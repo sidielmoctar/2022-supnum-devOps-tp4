be160341fd3b7e6b832db82f212e592c20e1caec
SIDI <a.sidielmoctar@gmail.com>
2022-12-07 11:29:02 +0000

    no-dupe-args test

--- src/js/test.js
+++ src/js/test.js
@@ -1,2 +1,10 @@
-let  i = 5
-console.log("un test ======== ", i);
+let  i = 5;
+
+// console.log("un test ======== ", i);
+
+function foo(a, b, a) {
+    console.log("value of the second a:", a);
+}
+
+
+foo(1,2,3);
