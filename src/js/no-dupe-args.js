4e56552443b6497848bc6635c1fe95abe248a0fa
SIDI <a.sidielmoctar@gmail.com>
2022-12-07 11:32:42 +0000

    suite

---
+++ src/js/no-dupe-args.js
@@ -0,0 +1,13 @@
+// function foo(a, b, a) {
+//     console.log("value of the second a:", a);
+// }
+
+
+// foo(1,2,3);
+
+function foo(a, b, c) {
+    console.log("value of the second a:", a);
+}
+
+
+foo(1,2,3);
---
+++ src/js/unused_var.js
@@ -0,0 +1,4 @@
+let  i = 5;
+
+// console.log("un test ======== ");
+console.log("un test ======== ", i);
